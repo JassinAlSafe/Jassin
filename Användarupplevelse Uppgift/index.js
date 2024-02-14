804d179078d9c676e84bb9cce37efc36d72f69ff
JassinAlSafe <121439769+Cakewhale97@users.noreply.github.com>
2024-02-14 13:17:07 +0100

    update

---
+++ Användarupplevelse Uppgift/index.js
@@ -0,0 +1,10 @@
+// const arrow = document.querySelector("#arrow");
+
+// arrow.addEventListener("click", () => {
+//     window.location.href = "/Main fixed/index.html"
+// });
+
+// const rent = document.getElementById("rentBtn");
+// rent.addEventListener("click", () => {
+//     window.location.href = "/cart fixed/cart.html";
+// });
